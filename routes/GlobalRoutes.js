10809f7e2e2c9f0bbf2fa9e591fd29b54ae7e394
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2024-06-23 07:43:29 +0530

    removed unused code

--- routes/GlobalRoutes.js
+++ routes/GlobalRoutes.js
@@ -1,35 +1,35 @@
 const express = require("express");
-const {
-  createStaff,
-  getAllStaff,
-  updateStaffById,
-  deleteStaffById,
-} = require("../controller/staff");
-const { validateDbId } = require("../middleware/middleware");
-const {
-  userRegisterValidation,
-  userLoginValidator,
-} = require("../utils/uservalidation");
-const { isAuthenticated } = require("../utils/auth");
-const {
-  registerUser,
-  loginUser,
-  getAllusers,
-  createorder,
-  getAllorder,
-} = require("../controller/user");
+// const {
+//   createStaff,
+//   getAllStaff,
+//   updateStaffById,
+//   deleteStaffById,
+// } = require("../controller/staff");
+// const { validateDbId } = require("../middleware/middleware");
+// const {
+//   userRegisterValidation,
+//   userLoginValidator,
+// } = require("../utils/uservalidation");
+// const { isAuthenticated } = require("../utils/auth");
+// const {
+//   registerUser,
+//   loginUser,
+//   getAllusers,
+//   createorder,
+//   getAllorder,
+// } = require("../controller/user");
 const { CreateResponse, getAllResponse } = require("../controller/response");
 const router = express.Router();
 
-router.post("/api/staff", createStaff);
-router.get("/api/staff", getAllStaff);
-router.put("/api/staff/:id", validateDbId, updateStaffById);
-router.delete("/api/staff/:id", validateDbId, deleteStaffById);
-// Users Routes
-router.post("/api/register", userRegisterValidation, registerUser);
-router.post("/api/login", userLoginValidator, loginUser);
-router.post("/api/order", createorder);
-router.get("/api/order", getAllorder);
+// router.post("/api/staff", createStaff);
+// router.get("/api/staff", getAllStaff);
+// router.put("/api/staff/:id", validateDbId, updateStaffById);
+// router.delete("/api/staff/:id", validateDbId, deleteStaffById);
+// // Users Routes
+// router.post("/api/register", userRegisterValidation, registerUser);
+// router.post("/api/login", userLoginValidator, loginUser);
+// router.post("/api/order", createorder);
+// router.get("/api/order", getAllorder);
 router.post("/hr/response", CreateResponse);
 router.get("/hr/response", getAllResponse);
 
